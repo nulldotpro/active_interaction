fff3075e2b67779acc6511e1720d51512ebd0561
Caleb Uriah Harrison <calebuharrison@gmail.com>
2020-12-01 16:07:04 -0600

    remove backports require statement

--- lib/active_interaction.rb
+++ lib/active_interaction.rb
@@ -49,7 +49,7 @@ require 'active_interaction/filters/time_filter'
 
 require 'active_interaction/base'
 
-require 'active_interaction/backports'
+# require 'active_interaction/backports'
 
 I18n.load_path.unshift(
   *Dir.glob(
